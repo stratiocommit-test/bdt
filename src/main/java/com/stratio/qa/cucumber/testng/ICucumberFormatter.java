8d26c9b4c87464c6a32b88436134f04ab4d56952
Alicia Doblas <adoblas@stratio.com>
2017-08-22 10:23:46 +0200

    [QATM-98] licensing repo following Stratio guide

--- src/main/java/com/stratio/qa/cucumber/testng/ICucumberFormatter.java
+++ src/main/java/com/stratio/qa/cucumber/testng/ICucumberFormatter.java
@@ -1,17 +1,9 @@
 /*
- * Copyright (C) 2014 Stratio (http://stratio.com)
+ * Copyright (c) 2014. Stratio Big Data Inc., Sucursal en España. All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This software is licensed under the Apache Licence 2.0. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the terms of the License for more details.
+ * SPDX-License-Identifier: Artistic-2.0
  */
 
 package com.stratio.qa.cucumber.testng;
