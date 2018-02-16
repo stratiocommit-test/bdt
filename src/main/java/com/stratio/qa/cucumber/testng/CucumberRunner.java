2d0c54ea238d385c180efc005fd9b31e530dc206
kai-stratio <kmohr@stratio.com>
2018-02-16 09:55:58 +0100

    added class path for postgres community in cucumber runner unique glue

--- src/main/java/com/stratio/qa/cucumber/testng/CucumberRunner.java
+++ src/main/java/com/stratio/qa/cucumber/testng/CucumberRunner.java
@@ -115,6 +115,8 @@ public class CucumberRunner {
         uniqueGlue.add("classpath:com/stratio/paas/hdfsframeworkAT/specs");
         uniqueGlue.add("classpath:com/stratio/paas/dgDatadictionaryAT/specs");
         uniqueGlue.add("classpath:com/stratio/paas/kafkaschemaregistryAT/specs");
+        uniqueGlue.add("classpath:com/stratio/postgresql/testsAT/specs");
+
         runtimeOptions.getGlue().clear();
         runtimeOptions.getGlue().addAll(uniqueGlue);
 
