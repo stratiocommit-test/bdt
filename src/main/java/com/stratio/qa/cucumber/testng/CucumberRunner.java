2ab85f7953ff72422e0c0b94512527192d6bb830
faluis-stratio <35024892+faluis-stratio@users.noreply.github.com>
2018-02-19 16:57:54 +0100

    [QATM-927] Subida de libreria de CucumberRunner (#284)
    * Subida de libreria de CucumberRunner
    
    * Empty commit to trigger rebuild

--- src/main/java/com/stratio/qa/cucumber/testng/CucumberRunner.java
+++ src/main/java/com/stratio/qa/cucumber/testng/CucumberRunner.java
@@ -119,6 +119,7 @@ public class CucumberRunner {
         uniqueGlue.add("classpath:com/stratio/cassandra/specs");
         uniqueGlue.add("classpath:com/stratio/schema_registry/specs");
         uniqueGlue.add("classpath:com/stratio/paas/sparkAT/specs");
+        uniqueGlue.add("classpath:com/stratio/schema/discovery/specs");
 
         runtimeOptions.getGlue().clear();
         runtimeOptions.getGlue().addAll(uniqueGlue);
